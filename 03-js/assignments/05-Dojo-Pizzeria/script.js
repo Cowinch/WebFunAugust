79f2c59369858f673f6ebcff0a3554341436da1e
Cole Winchester <cowinch@gmail.com>
2022-08-23 12:29:59 -0500

    pizzeria dojo complete

--- 03-js/assignments/05-Dojo-Pizzeria/script.js
+++ 03-js/assignments/05-Dojo-Pizzeria/script.js
@@ -1,20 +1,36 @@
-var pizza={
-    crustType: ["New York Style", "Chicago Style","Detroit Style", "St. Louis style", "Kansas City Style"],
-    cheese: ["Mozzarella", "Cheddar", "Provel", "Parmesan","Ricotta"],
-    sauceType:["Standard sauce","Sweet marina sauce","Barbeque sauce","Ranch","Pesto"],
-    protein: ["Pepperoni","Sausage","Bacon","Beef","Chicken"],
-    vegetables:["Green Pepper","Onion","Jalapeno","Black Olive","Mushroom"],
+var crustType= ["New York Style", "Chicago Style", "Detroit Style", "St. Louis style", "Kansas City Style"];
+var cheese= ["Mozzarella", "Cheddar", "Provel", "Parmesan", "Ricotta", "None"];
+var sauceType= ["Standard sauce", "Sweet marina sauce", "Barbeque sauce", "Ranch", "Pesto", "None"];
+var protein= ["Pepperoni", "Sausage", "Bacon", "Beef", "Chicken", "None"];
+var vegetables= ["Green Pepper", "Onion", "Jalapeno", "Black Olive", "Mushroom", "None"];
+
+function generateRandomNumber(min, max) {
+    Math.floor(Math.random() * (max - min) + min);
 }
-function generateRandomNumber(min,max){
-    Math.floor(Math.random()*(max-min+1)+min);
-}
-function pizzaOven(crust,cheese,sauce,meat,veggies){
-    var pizza={};
-    pizza.crustType=crust;
-    pizza.cheese=cheese;
-    pizza.sauceType=sauce;
-    pizza.protein=meat;
-    pizza.vegetables=veggies;
+function pizzaOven(crust, cheese, sauce, meat, veggies) {
+    var pizza = {};
+    pizza.crustType = crust;
+    pizza.cheese = cheese;
+    pizza.sauceType = sauce;
+    pizza.protein = meat;
+    pizza.vegetables = veggies;
     return pizza;
 }
-console.log(pizzaOven())
+// console.log(pizzaOven("Deep dish","Mozzarella","Standard sauce",["Pepperoni","Sausage"],"None"))
+// console.log(pizzaOven("Hand tossed",["Mozzarella","Feta"],"Standard sauce","None",["Mushrooms","Olvies","Onions"]))
+
+function pickRandomTopping(arr){
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+function randomPizza(){
+    var pizza = {};
+    pizza.crustType = pickRandomTopping(crustType);
+    pizza.cheese = pickRandomTopping(cheese);
+    pizza.sauceType = pickRandomTopping(sauceType);
+    pizza.protein = pickRandomTopping(protein);
+    pizza.vegetables = pickRandomTopping(vegetables);
+    return pizza;
+}
+console.log(randomPizza())
+
